a430e3e6366ff4c6e93aeb9e77d5deca8b8d498b
GVANTSAKHURTSID\Gvantsa <tornike.zhizhiashvili@geolab.edu.ge>
2018-12-25 18:35:29 +0400

    no message

--- js/main.js
+++ js/main.js
@@ -24,6 +24,51 @@ $('.home_main_slider').owlCarousel({
 })
 
 
+
+$('.home_gallery_slider').owlCarousel({
+    loop:true,
+    margin:0,
+    dots:false,
+    smartSpeed: 1500,
+    responsive:{
+        0:{
+            items:1
+        },
+        690:{
+            items:1
+        },
+        992:{
+            items:1
+        },
+        1200:{
+            items:1
+        }
+    }
+})
+
+$('.video_slider_wrapper').owlCarousel({
+    loop:true,
+    margin:5,
+    dots:false,
+    smartSpeed: 1500,
+    responsive:{
+        0:{
+            items:1
+        },
+        690:{
+            items:3
+        },
+        992:{
+            items:4
+        },
+        1200:{
+            items:5
+        }
+    }
+})
+
+
+
 // Scroll
 
 $(".srcroll_line a").on("click",function(e){
@@ -62,6 +107,87 @@ $(window).on("click",function(){
 
 $(".header_search").on("click",function(e){
     e.stopPropagation();
+})
+
+// Video Slider
+
+
+var vids = $(".home_video_box video"); 
+$.each(vids, function(){
+       this.controls = false; 
+}); 
+
+
+$(document).ready(function(){
+
+  $(".home_video_box video").mouseenter(function() {
+      this.play();
+
+  });
+
+  $(".home_video_box video").mouseleave(function() {
+      this.pause();
+  });
+
+})
+
+
+
+
+$(".home_video_box").mouseenter(function() {
+
+    $(this).find(".video_decor").addClass("active");
+    
+})
+
+$(".home_video_box").mouseleave(function() {
+    $(this).find(".video_decor").removeClass("active");
+})
+
+
+
+// modal video
+
+var vids = $(".home_video_modal video"); 
+$.each(vids, function(){
+       this.controls = false; 
+}); 
+//Loop though all Video tags and set Controls as false
+
+$(".home_video_modal video").click(function() {
+
+    
+  
+  if (this.paused) {
+    this.play();
+  } else {
+    this.pause();
+  }
+});
+
+
+// Video Modal
+
+$(".home_video_box video").on("click",function(){
+    $(".video_slider_modal").addClass("active");
+})
+
+$(".video_slider_modal").on("click",function(){
+   $(this).removeClass("active")
+})
+
+$(".home_video_modal").on("click",function(e){
+    e.stopPropagation();
+})
+
+
+
+$(window).on("load",function(){
+
+    var hh = $(".home_video_wrapper").height() / 1.6;
+
+    $(".home_video_text").css("height",hh);
+
 })
 
 
